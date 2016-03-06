b1f79975f2f0b4cd092d1e880c72e186f0526dae
cmrenus <cmrenus@gmail.com>
2016-03-06 14:28:01 -0500

    Set up of CAS in NodeJS maybe

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = angular.module('tmht', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
 	when('/', {
-		templateUrl: 'client/views/home.html'
+		templateUrl: 'client/views/home.html',
+		controller: 'loginCtrl'
+	}).
+	when('/landing', {
+		templateUrl: 'client/views/landing.html'
 	}).
 	otherwise({
 		redirectTo: '/'
