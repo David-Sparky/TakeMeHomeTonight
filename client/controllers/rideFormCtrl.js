dfcce767f5b7482f95da9a5718e94ed70d472bd3
cmrenus <cmrenus@gmail.com>
2016-03-18 16:18:38 -0400

    Ride Form html just about finished

--- client/controllers/rideFormCtrl.js
+++ client/controllers/rideFormCtrl.js
@@ -1,5 +1,21 @@
 angular.module('tmht')
-.controller('rideFormCtrl', ['$scope', function($scope){
+.controller('rideFormCtrl', ['$scope','$location', function($scope, $location){
 	
+	
+
+	if($location.path() == '/addRide'){
+		$scope.title = 'Add A Ride';
+		$scope.add = true;
+	}
+	else{
+		$scope.title = 'Request A Ride';
+		$scope.add = false;
+	}
+	console.log($scope.rideForm);
+
+	$scope.submit = function(){
+		console.log($scope.rideInfo);
+	}
+
 	
 }]);
