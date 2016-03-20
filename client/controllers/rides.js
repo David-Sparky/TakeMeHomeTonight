2c29b4d17c96959af284a26121862c3b2ac26116
minars <minars@rpi.edu>
2016-03-20 16:23:31 -0400

    Singular array for information

--- client/controllers/rides.js
+++ client/controllers/rides.js
@@ -4,19 +4,31 @@ angular.module('tmht')
 	if($location.path() == '/ridesoffered'){
 		$scope.needed = false;
 		$scope.ridesavail = true;
-			//All of these are just static arrays for the demo, once we get real data this will update with that. Until then this is what will be displaying
-		$scope.times = ['10:00pm','8:15am','2:30pm','5:20pm','7:30am','6:45pm','10:00am','12:00pm','6:45am','9:00pm'];
-		$scope.locations = ["Quad","Union","Union","ECAV","North Lot","Colonie Lot","15th Street Walk Over Bridge","Somehwere","Not here","Or There"];
-		$scope.seats = [2,3,4,2,1,1,0,2,1,0];
-		$scope.totalseats = [3,4,4,2,2,1,2,2,2,4];
-		$scope.cost = ['Free','Some Gas Money','Doesn\'t Matter','Free','$10','$5','Nothing','Free','Anything','Food'];	
-		$scope.names = ['Stephanie','Matt','Luis','Gunnar','Rick','Lindsay','Zack','Kelly','Audrie','Jacob'];
-		$scope.cars = ['2015 Honda Civic','2009 Hyundai Accent','2014 Ford Explorer','2015 Toyota Camry','Tesla Model S','2015 Audi A4','2012 BMW M2','2010 Ford F150','2009 Chevy Malibu','2015 Subaru WRX'];
-		$scope.colors = ['Blue','Gray','White','Gray','Red','Blue','Orange','Red','White','Blue'];
-
+		$scope.offerarray = [{Time:'10:00pm',Pickup:'Quad',Seats:'2',TotalSeats:'3',Cost:'Free',Name:'Stephanie',Car:'2015 Honda Civic',CarColor:'Blue',Destination:"Price Chopper"},
+												 {Time:'8:15am',Pickup:'Union',Seats:'3',TotalSeats:'4',Cost:'Some Gas Money',Name:'Matt',Car:'2009 Hyundai Accent',CarColor:'Gray',Destination:"Train Station"},
+												 {Time:'2:30pm',Pickup:'Union',Seats:'4',TotalSeats:'4',Cost:'Doesn\'t Matter',Name:'Luis',Car:'2014 Ford Explorer',CarColor:'White',Destination:"North Adams, MA"},
+												 {Time:'5:20pm',Pickup:'ECAV',Seats:'2',TotalSeats:'2',Cost:'Free',Name:'Gunnar',Car:'2015 Toyota Camry',CarColor:'Gray',Destination:"Walmart"},
+												 {Time:'7:30am',Pickup:'North Lot',Seats:'1',TotalSeats:'1',Cost:'$10',Name:'Rick',Car:'Tesla Model S',CarColor:'Red',Destination:"York, PA"},
+												 {Time:'6:45pm',Pickup:'Colonie Lot',Seats:'1',TotalSeats:'1',Cost:'$5',Name:'Lindsay',Car:'2015 Audi A4',CarColor:'Blue',Destination:"Albany Airport"},
+												 {Time:'10:00am',Pickup:'15th Street Walk Over Bridge',Seats:'2',TotalSeats:'3',Cost:'Nothing',Name:'Zack',Car:'2012 BMW M2',CarColor:'Orange',Destination:"Price Chopper"},
+												 {Time:'12:00pm',Pickup:'Somehwere',Seats:'2',TotalSeats:'2',Cost:'Free',Name:'Kelly',Car:'2010 Ford F150',CarColor:'Red',Destination:"Southern, MA"},
+												 {Time:'6:45am',Pickup:'Somehwere',Seats:'1',TotalSeats:'2',Cost:'Anything',Name:'Audrie',Car:'2009 Chevy Malibu',CarColor:'White',Destination:"Rhode Island"},
+												 {Time:'9:00pm',Pickup:'Somehwere',Seats:'0',TotalSeats:'4',Cost:'Food',Name:'Jacob',Car:'2015 Subaru WRX',CarColor:'Blue',Destination:"Anywhere"}
+												]
 	}
 	else{
 		$scope.needed = true;
 		$scope.ridesavail = false;
+		$scope.needarray =[{Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											 {Person:'name',Time:'time',DateToGo:'date',PlaceToGo:'place',Offer:'nothing'},
+											]
 	}
 }]);	
