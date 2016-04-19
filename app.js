bb11978ec37e3c62a598c712f6af8da150239768
David-Sparky <sparkd@rpi.edu>
2016-04-19 14:18:51 -0400

    Public Transit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.config(['$routeProvider', function($routeProvider){
 	when('/aboutUs', {
 		templateUrl: 'client/views/about-us.html'
 	}).
-	when('/publicTransit', {
+	when('/publicTrans', {
 		templateUrl: 'client/views/publicTrans.html',
 		controller: 'publicTransitCtrl'
 	}).
