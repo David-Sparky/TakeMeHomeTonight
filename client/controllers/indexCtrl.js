112f9402e370a82c16c15873e62f233fb63ff9da
rachelroks9 <rachelroks9@yahoo.com>
2016-05-01 11:47:31 -0400

    Collapse Nav Bar on Click

--- client/controllers/indexCtrl.js
+++ client/controllers/indexCtrl.js
@@ -12,4 +12,9 @@ angular.module('tmht')
 			return true;
 		}
 	};
+
+	$('.nav a').on('click', function(){
+	    $('.btn-navbar').click(); //bootstrap 2.x
+	    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+	});
 }]);
