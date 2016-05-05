7cb028892997592bf9c27e7b39c3108d9b2bae4e
cmrenus <cmrenus@gmail.com>
2016-05-04 23:07:59 -0400

    sockets

--- client/services/socketFactory.js
+++ client/services/socketFactory.js
@@ -2,7 +2,7 @@
 
 angular.module('tmht')
 .factory('socket', function (socketFactory) {
-    var myIoSocket = io.connect('https://takemehometonight.herokuapp.com');
+    var myIoSocket = io.connect('https://takemehometonight.herokuapp.com', {secure: true});
 
     var socket = socketFactory({
         ioSocket: myIoSocket
